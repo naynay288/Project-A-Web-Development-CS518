1404ea95c14f1de0057acfc1d05df42bebd309d9
Erica Haugh <33230570+naynay288@users.noreply.github.com>
2022-10-09 14:32:48 -0400

    Create navbar1.php

---
+++ navbar1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+	<div id="haut">
+		<ul class="menuhaut">
+			<li><a href="main1.php">Main</a></li>
+			<li><a href="profile1.php">Profile</a></li>
+			<li><a href="aboutUs1.php">About Us</a></li>
+		</ul>
+	</div>
+</html>
